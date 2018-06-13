ad7595f58bbe3f7e6b709ea242fe2b2bbf4febfe
robertHer <totolero769@gmail.com>
2018-06-13 16:52:19 -0700

    created 3 branches

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GamecontrolComponent } from './gamecontrol/gamecontrol.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamecontrolComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule
